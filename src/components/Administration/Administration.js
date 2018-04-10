f5dc2c33f9215755af577a741b96eb117fa7f2d8
Boris Lichtenstein <boimte@t-online.de>
2018-04-10 14:32:34 +0200

    created Administration component + Administration CSS

---
+++ src/components/Administration/Administration.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Administration.css';
+
+
+class Administration extends Component {
+  render() {
+    return (
+      <div className='Administration'>
+        <div className='Dashboard'>
+        </div>
+        <div className='Top-Header'>
+        DASHBOARD
+        </div>
+        <div className='Main'>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Administration;
