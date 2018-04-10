bc0567d489a233c237f526e01df9e703227b9096
Mohamed Saleh <mohmed_bakry51@yahoo.com>
2018-04-10 11:43:51 +0200

    add some comment

--- backend/index.js
+++ backend/index.js
@@ -21,21 +21,7 @@ mongoose.connection.once('open', () => {
     console.log("Successfully connected to the database");
 });
 
-app.post('/login', (req, res) => {
-    // Create and Save a new Student
-    if(!req.body) {
-        res.status(400).send({message: "admin can not be empty"});
-    }
-
-     admin = new Admin(req.body);
-      admin.save( (err) =>{
-        if(err) {
-          return res.send(err);
-        }
-        
-        return res.send({message: "admin created successfully!"})
-    });
-});
+require('./routes/adminLogin')(app);
 
 
 
