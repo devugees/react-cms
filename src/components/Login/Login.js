88bfd4279ee7fdc9b5445f869d1a8b2c7a62f0cb
Flukal <fluviobass@gmail.com>
2018-04-10 17:07:50 +0200

    Implemented onChange in input field

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,15 +3,52 @@ import './Login.css';
 
  class Login extends Component { 
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          loginData: {
+            email: "",
+            password: "",
+          }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      
+
+      handleChange(event) {
+        const loginDataCopy = {
+          ...this.state.loginData
+        };
+        loginDataCopy[event.target.name] = event.target.value;
+        this.setState({loginData: loginDataCopy});
+    
+      };
+
+      handleSubmit(e) {
+        e.preventDefault();
+        const data = {
+          email: this.state.loginData.email,
+          password: this.state.loginData.password
+        };
+    
+    
+        fetch.post('http://localhost:5000/login', data).then(function(response) {
+          console.log("Login successful: ", response);
+        }).catch(function(error) {
+          console.log("Error: ", error);
+        });
+      };
+
     render() {
         return(
-            <div class="Homepage">
+            <div className="Homepage">
                 <form>
-                    <div class="box">
+                    <div className="box">
                         <h1>Login</h1>
-                        <input type="email" placeholder="email" class="email" />
-                        <input type="password" placeholder="password" class="email" />
-                        <a href=""><div class="btn">Login</div></a> 
+                        <input type="email" name="email" placeholder="email" className="email" onChange ={this.handleChange} />
+                        <input type="password" name="password" placeholder="password" className="email" onChange ={this.handleChange} />
+                        <input type="submit" value="login" className="btn" onSubmit={this.handleSubmit} />
                     </div>
                 </form>
                 <p>Forgot your password? <u>Click Here!</u></p>
