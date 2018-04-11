4f36924e3564edd45b48a8fc9e36deefbe95da84
AmerBdx <amerbdx@gmail.com>
2018-04-11 15:49:44 +0200

    EditingDashBordStyle

--- src/components/dashBoard/dashBoard.js
+++ src/components/dashBoard/dashBoard.js
@@ -8,39 +8,43 @@ class SideMenu extends Component {
 
 
   
-	render() {
-		return(
-	 <div className="col-2 style">
+  render() {
+  const FontColor ={color: '#fff'};
+    return(
+  <div className="style">
         <strong>Dashbord</strong>
-        <Nav vertical>
+        <Nav className='color' vertical>
           <NavItem>
-            <NavLink href="#">Sitestatus</NavLink>
-          </NavItem>
+            <NavLink style={FontColor} href="#">Sitestatus</NavLink>
+            </NavItem>
           <NavItem>
-            <NavLink href="#">Update</NavLink>
+            <NavLink style={FontColor} href="#">Update</NavLink>
           </NavItem>
         </Nav>
+
         <hr />
 
         <strong>Site Structure</strong>
-        <div className="nomargin">
-         <Nav vertical>
-          <NavItem>
-            <NavLink href="/contentType">Content Type</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/Menues">Menues</NavLink>
-          </NavItem>
-           <NavItem>
-            <NavLink href="/allFields">All Fileds</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Field Type</NavLink>
-          </NavItem>
-          </Nav>
+          <div>
+            <Nav vertical>
+              <NavItem>
+                <NavLink style={FontColor} href="/contentType">Content Type</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="/Menues">Menues</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="/allFields">All Fileds</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Field Type</NavLink>
+              </NavItem>
+            </Nav>
           </div>
-            <hr />
-            <div>
+          
+        <hr />
+          
+          <div>
             {/* here will implent the post  
                    <strong>Post</strong>
                   <Nav vertical>
@@ -57,52 +61,57 @@ class SideMenu extends Component {
                   <NavLink href="#">Link</NavLink>
                 </NavItem>
                 </Nav>*/}  
-             </div>
-             <hr />
-         <strong>Appearance</strong>
-          <Nav vertical>
-          <NavItem>
-            <NavLink href="#">Themes</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Custome</NavLink>
-          </NavItem>
-           <NavItem>
-            <NavLink href="#">Blocks</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Custome Code</NavLink>
-          </NavItem>
-           <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          </Nav>
-              <hr />
-           <strong>Users</strong>
-         <Nav vertical>
-          <NavItem>
-            <NavLink href="#">ALl Users</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Roles</NavLink>
-          </NavItem>
-          </Nav>
-          <hr />
-           <Nav vertical>
-            <NavItem>
-            <NavLink href="#">Setting</NavLink>
-          </NavItem>
-          </Nav>
-           <hr />
+          </div>
+        <hr />
+        <strong>Appearance</strong>
             <Nav vertical>
-            <NavItem>
-            <NavLink href="#">Plugins</NavLink>
-          </NavItem>
-          </Nav>
-      </div>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Themes</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Custome</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Blocks</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Custome Code</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Link</NavLink>
+              </NavItem>
+            </Nav>
 
-		);
-	}
+        <hr />
+
+        <strong>Users</strong>
+            <Nav vertical>
+              <NavItem>
+                <NavLink style={FontColor} href="#">All Users</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Roles</NavLink>
+              </NavItem>
+            </Nav>
+        <hr />
+           
+            <Nav vertical>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Setting</NavLink>
+              </NavItem>
+            </Nav>
+
+        <hr />
+
+            <Nav vertical>
+              <NavItem>
+                <NavLink style={FontColor} href="#">Plugins</NavLink>
+              </NavItem>
+            </Nav>
+  </div>
+
+    );
+  }
 }
 
 export default SideMenu;
