252213257efc8b10fb736c9dc2deebc87e4d8981
varg666 <jemy222@hotmail.com>
2018-04-12 11:10:20 +0200

    settings components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/HeaderComponent/HeaderComponent';
+import Settings from './components/SettingsComponent/SettingsComponent'
 import DashBoard from './components/dashBoard/dashBoard';
 import './components/dashBoard/dashBoard.css';
 import Login from './components/Login/Login';
@@ -17,11 +18,11 @@ class App extends Component {
     	<div>
     	<Header />
     	<DashBoard />
-       
+      <Settings />
              <BrowserRouter>
               <div className="container">
                  <div className="sub">
-	                 
+                  
 	                  <Route exact path="/contentType" component={contentType} />
 	                  <Route exact path="/Menues" component={Menues} />
 	                  <Route exact path="/allFields" component={allFields} />
