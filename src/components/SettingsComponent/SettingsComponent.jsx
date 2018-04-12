135a576b173c00382ad87f7398d681e1707efc6a
Licht(en)Stein <29630385+Licht-en-Stein@users.noreply.github.com>
2018-04-12 11:39:44 +0200

    Update SettingsComponent.jsx

--- src/components/SettingsComponent/SettingsComponent.jsx
+++ src/components/SettingsComponent/SettingsComponent.jsx
@@ -5,21 +5,7 @@ import './SettingsComponent.css';
 
 
 export default class Example extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      startDate: moment()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
  
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
-  }
-
-
     render() {
         return (
                   
