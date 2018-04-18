7aee1390be1859d860e00938656b55b2145df08e
Unknown <nebras.z.othman@gmail.com>
2018-04-18 16:03:55 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Routers from './Routers';
 import Administration from './components/Administration/Administration';
 
 
@@ -9,23 +10,11 @@ import Administration from './components/Administration/Administration';
 
 class App extends Component {
 
-
-
   render() {
 
     return (
-
-    	<div>
-             <BrowserRouter>
-                 <div className="sub">
-                    <Route  exact path="/Administration/" component={Administration} />
-                    <Route  path="/Administration/:activeLink" component={Administration} />
-	                
-               </div>
-              
-             </BrowserRouter>
-		  </div>
-    );
+              <Routers/>
+      );
   }
 }
 
