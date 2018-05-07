ab8fe9493a2ed34ce63e35836cdb374f1ba210be
Unknown <nebras.z.othman@gmail.com>
2018-05-07 12:54:48 +0200

    Stil working on show the entries

--- src/components/ContentTypesPanel/ContentTypePanel.js
+++ src/components/ContentTypesPanel/ContentTypePanel.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import './ContentTypePanel.css';
 import AddEntrie from './AddEntrie/AddEntrie'
+import axios from 'axios';
+
 
   class ContentTypePanel extends Component {
 
+        constructor(props) {
+          super(props);
+                this.state = {
+                  entries:[],
+                  entriesKeys:[]
+                };
+        }
+
+   
+
+entries = [];
+
+    componentWillMount = () => {
+                axios.get(`http://localhost:5000/api/entries/${this.props.id}`)
+              .then((response) => {
+                response.data.map((entrie) => {
+                  this.entries.push(entrie.content)
+                })
+                
+                
+                 console.log(this.entries);
+                }).catch(function(error) {
+                  console.log("Error: ", error);
+                });
+     
+    }
+
+
+
   render() {
+     
+   
+    console.log(this.entries)
+
+         
+    
     return (
       <div>
-          <h1>ContentTypePanel Component</h1>
+          <h1>Hoii </h1>
           <AddEntrie fields={this.props.fields}/>
       </div>
     );
