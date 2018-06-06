161e2535b685d4f98fde6eb91530faf36a8eadd0
Flukal <fluviobass@gmail.com>
2018-06-06 14:24:16 +0200

    Custom Form & Footer on LandingPage

--- src/components/Appearance/Custome/Custome.js
+++ src/components/Appearance/Custome/Custome.js
@@ -15,7 +15,7 @@ class Custome extends Component {
   render() {
 
     const style = { marginBottom: '1em' }
-    const btnFile = { marginBottom: '1em', height: '2.8em' }
+    const btnFile = { marginBottom: '1em', height: '2em' }
 
     const options = this.state.categories.map((item, index) => (
       {
@@ -28,7 +28,7 @@ class Custome extends Component {
       <Form>
           <Row>
             <Col>
-              <h1>Menu</h1>
+              <h3>Menu</h3>
               <Input style={style} placeholder="Website Title" />
               <Select 
                 style={style}
@@ -39,24 +39,45 @@ class Custome extends Component {
                 options={options} 
                 onChange={this.handleSelectChange} />
             </Col>
-            <Col>
-              <h1>Footer</h1>
-              <Input style={style} placeholder="Copyright" />
-            </Col>
           </Row>
-          
           <Row>
             <Col>
-              <h1>IconBox</h1>
+              <h3>IconBox Left </h3>
               <Input style={style} placeholder="Icon Name" />
               <Input style={style} placeholder="Title" />
               <Input style={style} placeholder="Text" />
             </Col>
             <Col>
-              <h1>Post</h1>
+              <h3>IconBox Center</h3>
+              <Input style={style} placeholder="Icon Name" />
+              <Input style={style} placeholder="Title" />
+              <Input style={style} placeholder="Text" />
+            </Col>
+            <Col>
+              <h3>IconBox Right</h3>
+              <Input style={style} placeholder="Icon Name" />
+              <Input style={style} placeholder="Title" />
+              <Input style={style} placeholder="Text" />
+            </Col>
+          </Row>
+          <Row>
+          <Col>
+              <h3>Post</h3>
               <Input style={btnFile} type="file" name="file" id="exampleFile" />
               <Input style={style} placeholder="Title" />
               <Input style={style} placeholder="Text" />
+            </Col>
+            <Col>
+              <h3>Footer</h3>
+              <Input style={style} placeholder="Copyright" />
+              <Select 
+                style={style}
+                multi
+                joinValues 
+                value={this.state.value}
+                placeholder="Choose Pages for Footer"
+                options={options} 
+                onChange={this.handleSelectChange} />
             </Col>
           </Row>
       </Form>
--- src/components/LandingPage/LayoutComponents/Footer.js
+++ src/components/LandingPage/LayoutComponents/Footer.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import '../LandingPage.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+import { white } from 'material-ui/styles/colors';
 
 
 
 
 export default class Footer extends React.Component {
     render(){
+      
+      const style = {color: 'white', height: '2.5em', marginTop: '1em'}
+      const Copyright = {fontSize: '1em'}
+
       return (
-        <footer >
-          <p>Consectetuer</p>
-        </footer>
+        <Navbar className="Navbar" dark expand="md" style={style}>
+          <NavbarBrand style={Copyright}>Copyright</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">AGB</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Privacy Policy</NavLink>
+              </NavItem>
+            </Nav>
+          
+        </Navbar>
       );
     }
 }
