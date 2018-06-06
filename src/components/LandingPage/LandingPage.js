5f761d049199b743eb692b3440854b6a1dca51b6
Flukal <fluviobass@gmail.com>
2018-06-06 10:15:35 +0200

    Errors and alerts deleted and code cleaned.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
-import {
-    Container,
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    Row,
-    Jumbotron,
-    Col,
-    DropdownItem } from 'reactstrap';
+import { Container } from 'reactstrap';
 import Menu from './LayoutComponents/Menu';
 import Slide from './LayoutComponents/Slide';
 import IconBox from './LayoutComponents/IconBox';
 import Post from './LayoutComponents/Post';
 import Footer from './LayoutComponents/Footer';
 import './LandingPage.css';
-import axios from 'axios';
-//import * as Icon from 'react-icons/lib/fa';
 
 class LandingPage extends Component {
 
@@ -161,138 +144,13 @@ class LandingPage extends Component {
         margin: '0',
         padding: '0'
       };
-      /*
-    const postBox = {
-        margin: '0', 
-        padding: '0',
-        marginTop: '1em'
-      };
-*/
-    const  hrBorder = {
-      margin: '0em 25em',
-      borderColor: 'grey',
-      marginTop: '2em'
-    } 
-/*
-    const android = <Icon.FaAndroid size='120' className="Icon" />
-    const github = <Icon.FaGithubAlt size='120' className="Icon" />
-    const paw = <Icon.FaPaw size='120' className="Icon" />
-
-    const columns = [
-      {
-        icon: android,
-        title: "Lorem ipsum",
-        text: "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
-      },
-      {
-        icon: github,
-        title: "Nam liber",
-        text: "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-      },
-      {
-        icon: paw,
-        title: "Duis autem",
-        text: "Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
-      }
-    ]
-    */
-    
-
- 
 
     return (
       <Container style={landingPage} fluid>
-      <Menu />
-        {/* <Navbar className="Navbar" dark expand="md">
-          <NavbarBrand href="/">KinoView</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/login">Login</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar> */}
+        <Menu />
         <Slide />
-        {/*<Jumbotron fluid className="Jumbotron">
-            <Container fluid>
-            <h1 className="display-3">Welcome to the LandingPage</h1>
-            <p className="lead">React CMS !</p>
-            </Container>
-          </Jumbotron> */}
-
-        {/* <Row style={landingPage}>
-          <Col className="Box">
-            <Icon.FaAndroid size='120' className="Icon" />
-            <h3>Lorem ipsum</h3>
-            <p><footer >
-          <p>Consectetuer</p>
-        </footer>t ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-          </Col<footer >
-          <p>Consectetuer</p>
-        </footer>
-          <Col <footer >
-          <p>Consectetuer</p>
-        </footer>
-            <Ic<footer >
-          <p>Consectetuer</p>
-        </footer>
-            <h3>Nam liber</h3>
-            <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
-          </Col>
-          <Col className="Box">
-            <Icon.FaPaw size='120' className="Icon" />
-            <h3>Duis autem</h3>
-            <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-          </Col>
-    </Row> */}
-    <IconBox />
-   {/*
-        <Row style={landingPage}> 
-          {columns.map(column => {
-            console.log(column)
-            return(
-              <Col className="Box">
-                {column.icon}
-                <h3>{column.title}</h3>
-                <p>{column.text}</p>
-               </Col>  
-            )<footer >
-          <p>Consectetuer</p>
-        </footer>
-          })}      
-        </Row>       
- 
-        <hr style={hrBorder}/>*/}
+        <IconBox />
         <Post />
-        {/*<Row style={postBox} className="text-center">
-            <Col sm="8">
-              <h3>Duis autem</h3>
-              <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-            </Col>
-            <Col sm="4">
-            <img src={require('./image/tool.jpg')} />
-            </Col>
-        </Row>
-
-        <hr style={hrBorder}/>
-        <Row style={postBox} className="text-center">
-            <Col sm="4">
-              <img src={require('./image/ananas.jpg')} />
-            </Col>
-            <Col sm="8">
-              <h3>Duis autem</h3>
-              <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-            </Col>
-        </Row> */}
-
-        {/*<footer >
-          <p>Consectetuer</p>
-        </footer>*/}
         <Footer />
       </Container>
     );
