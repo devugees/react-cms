8a524b5e7be1b2526495377bca0145a6142050b8
Boris Lichtenstein <boimte@t-online.de>
2018-06-07 15:25:44 +0200

    adding propTypes and cleaned and indent code

--- src/components/Administration/dashBoard/dashBoard.js
+++ src/components/Administration/dashBoard/dashBoard.js
@@ -1,135 +1,181 @@
 import React, {Component} from 'react';
-import './dashBoard.css'
-import { Nav, NavItem} from 'reactstrap';
+import PropTypes from 'prop-types';
+import './dashBoard.css';
+import {Nav, NavItem} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 class dashBord extends Component {
+  state = {
+    contentTypes: this.props.contenttypes
+  };
 
-        state = {
-         contentTypes: this.props.contenttypes
+  static PropTypes = {
+    contenttypes: PropTypes.array
+  };
+
+  render() {
+    let recive = this.props.contenttypes;
+    let contentTypesObj = null;
+    contentTypesObj = (
+      <div>
+        {recive.map((contentType, index) => {
+          if (!contentType) {
+            return;
           }
+          return (
+            <div key={index}>
+              <strong>{contentType.title}</strong>
+              <Nav vertical>
+                <NavItem>
+                  <Link
+                    className="dashLink"
+                    to={`/Administration/ContentType/${contentType._id}`}>
+                    All {contentType.title}
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link
+                    className="dashLink"
+                    to={`/Administration/Categories/${contentType._id}`}>
+                    Categories {contentType.title}
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link
+                    className="dashLink"
+                    to={`/Administration/Structure/${contentType._id}`}>
+                    Structure
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link
+                    className="dashLink"
+                    to={`/Administration/View/${contentType._id}`}>
+                    View
+                  </Link>
+                </NavItem>
+              </Nav>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    );
 
-  
-render() {
-  let recive = this.props.contenttypes;
-let contentTypesObj = null;
-        contentTypesObj = (
-             <div>
-                {recive.map((contentType, index) => {
-                   if(!contentType){
-                    return;
-                   }
-                     return (
-                           <div key={index}>
-                             <strong>{contentType.title}</strong>
-                             <Nav vertical>
-                            <NavItem >
-                              <Link className='dashLink' to={`/Administration/ContentType/${contentType._id}`}>All {contentType.title}</Link>
-                            </NavItem>
-                            <NavItem>
-                              <Link className='dashLink' to={`/Administration/Categories/${contentType._id}`}>Categories {contentType.title}</Link>
-                            </NavItem>
-                             <NavItem>
-                              <Link className='dashLink' to={`/Administration/Structure/${contentType._id}`}>Structure</Link>
-                            </NavItem>
-                            <NavItem>
-                              <Link className='dashLink' to={`/Administration/View/${contentType._id}` }>View</Link>
-                            </NavItem>
-                            </Nav>
-                             <hr />
-                        </div>
+    return (
+      <div className="style FontColor">
+        <strong>Dashbord</strong>
+        <Nav className="color" vertical>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Sitestatus">
+              Site Status
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Update">
+              Update
+            </Link>
+          </NavItem>
+        </Nav>
 
-                      );
-                   
-                })}   
-             </div>
-          )
+        <hr />
 
-return(
-<div className="style FontColor">
-  <strong>Dashbord</strong>
-  <Nav className='color' vertical>
-    <NavItem>
-      <Link className='dashLinks'  to="/Administration/main/Sitestatus">Site Status</Link>
-      </NavItem>
-    <NavItem>
-      <Link className='dashLinks'  to="/Administration/main/Update">Update</Link>
-    </NavItem>
-  </Nav>
-
-  <hr />
-
-  <strong>Site Structure</strong>
-    <div>
-      <Nav vertical>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/ContentTypesList">Content Types</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Menues">Menues</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/AllFields">All Fileds</Link>
-        </NavItem>
-        {/*<NavItem>
+        <strong>Site Structure</strong>
+        <div>
+          <Nav vertical>
+            <NavItem>
+              <Link
+                className="dashLinks"
+                to="/Administration/main/ContentTypesList">
+                Content Types
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="dashLinks" to="/Administration/main/Menues">
+                Menues
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="dashLinks" to="/Administration/main/AllFields">
+                All Fileds
+              </Link>
+            </NavItem>
+            {/*<NavItem>
           <Link className='dashLinks'  to="/Administration/main/FieldTypes">Field Types</Link>
         </NavItem>*/}
-      </Nav>
-    </div>
-    
-  <hr />
-  <div>
-    {contentTypesObj ? contentTypesObj : null}
-    </div>
-  <strong>Appearance</strong>
-      <Nav vertical>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Themes">Themes</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Custome">Custom</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Blocks">Blocks</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/CustomeCode">Custom Code</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Editor">Editor</Link>
-        </NavItem>
-      </Nav>
+          </Nav>
+        </div>
 
-  <hr />
+        <hr />
+        <div>{contentTypesObj ? contentTypesObj : null}</div>
+        <strong>Appearance</strong>
+        <Nav vertical>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Themes">
+              Themes
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Custome">
+              Custom
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Blocks">
+              Blocks
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/CustomeCode">
+              Custom Code
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Editor">
+              Editor
+            </Link>
+          </NavItem>
+        </Nav>
 
-  <strong>Users</strong>
-      <Nav vertical>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/AllUsers">All Users</Link>
-        </NavItem>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Roles">Roles</Link>
-        </NavItem>
-      </Nav>
-  <hr />
-     
-      <Nav vertical>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/SettingsComponent">Setting</Link>
-        </NavItem>
-      </Nav>
+        <hr />
 
-  <hr />
+        <strong>Users</strong>
+        <Nav vertical>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/AllUsers">
+              All Users
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Roles">
+              Roles
+            </Link>
+          </NavItem>
+        </Nav>
+        <hr />
 
-      <Nav vertical>
-        <NavItem>
-          <Link className='dashLinks'  to="/Administration/main/Plugins">Plugins</Link>
-        </NavItem>
-      </Nav>
-</div>
+        <Nav vertical>
+          <NavItem>
+            <Link
+              className="dashLinks"
+              to="/Administration/main/SettingsComponent">
+              Setting
+            </Link>
+          </NavItem>
+        </Nav>
 
-);
-}
+        <hr />
+
+        <Nav vertical>
+          <NavItem>
+            <Link className="dashLinks" to="/Administration/main/Plugins">
+              Plugins
+            </Link>
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
 }
 
 export default dashBord;
