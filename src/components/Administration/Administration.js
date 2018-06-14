5f83cfd03abf6d208d330c55884076fb569a47cf
Licht(en)Stein <boimte@t-online.de>
2018-06-14 17:23:23 +0200

    added dynamic links from components and all users, added PropTypes

--- src/components/Administration/Administration.js
+++ src/components/Administration/Administration.js
@@ -11,20 +11,20 @@ class Administration extends Component {
   constructor() {
     super();
     this.state = {
-      contentTypes: [],
+      contentTypes: []
     };
   }
 
   static propTypes = {
     activeLink: PropTypes.string
   };
-   //NewHook
+  //NewHook
   componentDidMount = () => {
     axios
       .get('http://localhost:5000/api/contenttypes')
       .then(response => {
         this.setState({
-          contentTypes: response.data,
+          contentTypes: response.data
         });
       })
       .catch(function(error) {
@@ -34,25 +34,26 @@ class Administration extends Component {
 
   render() {
     const colstyle = {padding: '0', margin: '0 0 0 0'};
-    const padding = {padding: '1em', margin: 0}
+    const padding = {padding: '1em', margin: 0};
     return (
-      <div className="Administration" >
+      <div className="Administration">
         <Row style={colstyle}>
-          <Col style={colstyle} >
-            <TopHeader history={this.props.history}/>
-        
+          <Col style={colstyle}>
+            <TopHeader
+              history={this.props.history}
+              contentTypes={this.state.contentTypes}
+            />
           </Col>
         </Row>
-        <Row style={colstyle} >
-          <Col md="2" style={colstyle} >
+        <Row style={colstyle}>
+          <Col md="2" style={colstyle}>
             <DashBoard contenttypes={this.state.contentTypes} />
           </Col>
-          <Col className="Board" md="10" style={padding} >
+          <Col className="Board" md="10" style={padding}>
             <Main
               contenttypes={this.state.contentTypes}
               className="Main"
               activeLink={this.props.match.url}
-
             />
           </Col>
         </Row>
