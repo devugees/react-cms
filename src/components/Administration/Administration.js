e3a470b77eecb9a58eb2694a68ff02deb32d2cbb
Licht(en)Stein <boimte@t-online.de>
2018-06-14 17:23:23 +0200

    added dynamic links from components and all users

--- src/components/Administration/Administration.js
+++ src/components/Administration/Administration.js
@@ -35,29 +35,21 @@ class Administration extends Component {
   render() {
     const colstyle = {padding: '0', margin: '0 0 0 0'};
     const padding = {padding: '1em', margin: 0}
-    return (
-      <div className="Administration" >
+    return <div className="Administration">
         <Row style={colstyle}>
-          <Col style={colstyle} >
-            <TopHeader history={this.props.history}/>
-        
+          <Col style={colstyle}>
+          <TopHeader history={this.props.history} contentTypes={this.state.contentTypes}/>
           </Col>
         </Row>
-        <Row style={colstyle} >
-          <Col md="2" style={colstyle} >
+        <Row style={colstyle}>
+          <Col md="2" style={colstyle}>
             <DashBoard contenttypes={this.state.contentTypes} />
           </Col>
-          <Col className="Board" md="10" style={padding} >
-            <Main
-              contenttypes={this.state.contentTypes}
-              className="Main"
-              activeLink={this.props.match.url}
-
-            />
+          <Col className="Board" md="10" style={padding}>
+            <Main contenttypes={this.state.contentTypes} className="Main" activeLink={this.props.match.url} />
           </Col>
         </Row>
-      </div>
-    );
+      </div>;
   }
 }
 
--- src/components/Administration/HeaderComponent/HeaderComponent.js
+++ src/components/Administration/HeaderComponent/HeaderComponent.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 import './HeaderComponent.css';
 import {Link} from 'react-router-dom';
-import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, } from 'reactstrap';
+import {
+  Button,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
 class TopHeader extends Component {
   constructor() {
@@ -9,53 +21,74 @@ class TopHeader extends Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false,
+      isOpen: false
     };
-    this.handleLogout = this.handleLogout.bind(this)
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen,
+      isOpen: !this.state.isOpen
     });
   }
 
   handleLogout() {
-    this.props.history.push("/Login");
-    localStorage.clear("token")
+    this.props.history.push('/Login');
+    localStorage.clear('token');
   }
 
   render() {
-    const marginRight = {marginRight: '1em'}
-    const textStyle = {color: 'black', fontSize: '14px', fontWeight: 'bold'}
-    const linkStyle = {textDecoration: 'none'}
+    const marginRight = {marginRight: '1em'};
+
     return (
       <div className="top" style={{display: 'inline'}}>
-        <Navbar className="Navbar" dark expand="md" style={{background: '#85C1E9', height: '5vh', zIndex: 100}}>
-          <NavbarBrand style={textStyle} href="/">Dashbord</NavbarBrand>
-
+        <Navbar className="Navbar" dark expand="md">
+          <NavbarBrand href="/">Dashbord</NavbarBrand>
 
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-        
-        <NavbarBrand style={{display:'flex', padding: '0', margin: 'auto'}}>
-              <NavItem style={marginRight}><Link style={linkStyle, textStyle} to="/">Go to Web Site</Link></NavItem>
-              <NavItem style={marginRight}><Link style={linkStyle, textStyle} to="/Administration/main/NewContentType">Create Node</Link></NavItem>
-        </NavbarBrand>
-       <Navbar>
-        <UncontrolledDropdown nav inNavbar style={marginRight}>
-          <DropdownToggle style={textStyle} nav caret>New</DropdownToggle>
-            <DropdownMenu right style={{top: '4vh'}}>
-              <DropdownItem>Option 1</DropdownItem>
-              <DropdownItem>Option 2</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-        </UncontrolledDropdown>
-        </Navbar>
-          <Button style={{padding: '0.1em 0.4em', margin: '3% 0', fontSize: '14px', height: '1%'}} onClick={this.handleLogout}>Sign out </Button>
+              <NavbarBrand
+                style={{display: 'flex', padding: '.6em 0 0 0', margin: '0'}}>
+                <NavItem style={marginRight}>
+                  <Link to="/">Go to Web Site</Link>
+                </NavItem>
+                <NavItem style={marginRight}>
+                  <Link to="/Administration/main/NewContentType">
+                    Create Node
+                  </Link>
+                </NavItem>
+              </NavbarBrand>
 
+              <NavbarBrand>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    New
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    {this.props.contentTypes.map((content, index) => {
+                      return (
+                        <DropdownItem>
+                          <Link
+                            to={`/Administration/ContentType/${content._id}`}
+                            key={index}>
+                            {content.title}
+                          </Link>
+                        </DropdownItem>
+                      );
+                    })}
+                    <DropdownItem>
+                      <Link to="/Administration/main/AllUsers">Users</Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </NavbarBrand>
+
+              <NavbarBrand>
+                <Button style={marginRight} onClick={this.handleLogout}>
+                  Sign out{' '}
+                </Button>
+              </NavbarBrand>
 
               {/* <UncontrolledDropdown>
                 <DropdownToggle style={textColor} caret>
@@ -77,9 +110,6 @@ class TopHeader extends Component {
                 </DropdownMenu>
               </UncontrolledDropdown>
               */}
-
-
-
             </Nav>
           </Collapse>
         </Navbar>
