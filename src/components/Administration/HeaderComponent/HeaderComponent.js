352ceff7744b645f04528d5a2cf71f9ab3b6886b
Nebras-othman <nebras.z.othman@gmail.com>
2018-06-14 18:35:53 +0200

    Fixing Merge bug after the styling of Amer

--- src/components/Administration/HeaderComponent/HeaderComponent.js
+++ src/components/Administration/HeaderComponent/HeaderComponent.js
@@ -1,19 +1,7 @@
 import React, {Component} from 'react';
 import './HeaderComponent.css';
 import {Link} from 'react-router-dom';
-import {
-  Button,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, } from 'reactstrap';
 
 class TopHeader extends Component {
   constructor() {
@@ -21,46 +9,41 @@ class TopHeader extends Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
-    this.handleLogout = this.handleLogout.bind(this);
+    this.handleLogout = this.handleLogout.bind(this)
   }
 
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
     });
   }
 
   handleLogout() {
-    this.props.history.push('/Login');
-    localStorage.clear('token');
+    this.props.history.push("/Login");
+    localStorage.clear("token")
   }
 
   render() {
-    const marginRight = {marginRight: '1em'};
-
+    const marginRight = {marginRight: '1em'}
+    const textStyle = {color: 'black', fontSize: '14px', fontWeight: 'bold'}
+    const linkStyle = {textDecoration: 'none'}
     return (
       <div className="top" style={{display: 'inline'}}>
-        <Navbar className="Navbar" dark expand="md">
-          <NavbarBrand href="/">Dashbord</NavbarBrand>
+        <Navbar className="Navbar" dark expand="md" style={{background: '#85C1E9', height: '5vh', zIndex: 100}}>
+          <NavbarBrand style={textStyle} href="/">Dashbord</NavbarBrand>
+
 
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavbarBrand
-                style={{display: 'flex', padding: '.6em 0 0 0', margin: '0'}}>
-                <NavItem style={marginRight}>
-                  <Link to="/">Go to Web Site</Link>
-                </NavItem>
-                <NavItem style={marginRight}>
-                  <Link to="/Administration/main/NewContentType">
-                    Create Node
-                  </Link>
-                </NavItem>
-              </NavbarBrand>
-
-              <NavbarBrand>
+        
+        <NavbarBrand style={{display:'flex', padding: '0', margin: 'auto'}}>
+              <NavItem style={marginRight}><Link style={linkStyle, textStyle} to="/">Go to Web Site</Link></NavItem>
+              <NavItem style={marginRight}><Link style={linkStyle, textStyle} to="/Administration/main/NewContentType">Create Node</Link></NavItem>
+        </NavbarBrand>
+        <NavbarBrand>
                 <UncontrolledDropdown nav inNavbar>
                   <DropdownToggle nav caret>
                     New
@@ -82,13 +65,9 @@ class TopHeader extends Component {
                     </DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
-              </NavbarBrand>
+          </NavbarBrand>
+          <Button style={{padding: '0.1em 0.4em', margin: '3% 0', fontSize: '14px', height: '1%'}} onClick={this.handleLogout}>Sign out </Button>
 
-              <NavbarBrand>
-                <Button style={marginRight} onClick={this.handleLogout}>
-                  Sign out{' '}
-                </Button>
-              </NavbarBrand>
 
               {/* <UncontrolledDropdown>
                 <DropdownToggle style={textColor} caret>
@@ -110,6 +89,9 @@ class TopHeader extends Component {
                 </DropdownMenu>
               </UncontrolledDropdown>
               */}
+
+
+
             </Nav>
           </Collapse>
         </Navbar>
