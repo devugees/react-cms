819cdf984b7dc7256235944a5220546c62c3c892
Flukal <fluviobass@gmail.com>
2018-06-18 11:43:57 +0200

    Sticked HeadTable in ViewTable component

--- src/components/ViewTable/ViewTable.js
+++ src/components/ViewTable/ViewTable.js
@@ -1,16 +1,12 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import './ViewTable.css';
-import {Table, Row} from 'reactstrap';
-import axios from 'axios';
-
-
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./ViewTable.css";
+import { Table, Row } from "reactstrap";
+import axios from "axios";
 
 class ViewTable extends Component {
   constructor(props) {
     super(props);
-
   }
 
   static propTypes = {
@@ -23,121 +19,123 @@ class ViewTable extends Component {
 
   handleEdit = (index, machineName) => {
     this.props.toggle();
-    const item = this.props.items[index]
-    this.props.bringItemWillBeEditedFromViewTable(item,index)
-
-   
-  }
+    const item = this.props.items[index];
+    this.props.bringItemWillBeEditedFromViewTable(item, index);
+  };
 
   handleDelete = (index, machineName) => {
-
-    if(!machineName) {
-    let itemId = this.props.itemsWithId[index]._id
-    console.log(itemId);
-    axios.delete(`http://localhost:5000/api/entries/${itemId}`)
-    .then(response => {
-      console.log(response);
-      if(response.data.message) {
-          console.error(response.data.message)
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error)
-      })
-      this.props.deleteEntrieFromState(index)
-      }else {
-        this.props.deleteFieldFromState(index)
-      }
-
+    if (!machineName) {
+      let itemId = this.props.itemsWithId[index]._id;
+      console.log(itemId);
+      axios
+        .delete(`http://localhost:5000/api/entries/${itemId}`)
+        .then(response => {
+          console.log(response);
+          if (response.data.message) {
+            console.error(response.data.message);
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+      this.props.deleteEntrieFromState(index);
+    } else {
+      this.props.deleteFieldFromState(index);
     }
+  };
 
-renderedKeys
-renderedItems
+  renderedKeys;
+  renderedItems;
 
   render() {
-
     if (!this.props.keys) {
-      return null
+      return null;
     }
 
     this.renderedKeys = Object.keys(this.props.keys).map((key, index) => {
-                return <th key={index}>{key}</th>
-    })
+      return <th key={index}>{key}</th>;
+    });
 
-    let values
-    if(this.props.items.length > 0) {
-       
-        
-        this.renderedItems = this.props.items.map((item, index) => {
-          values = Object.values(item)
-            return (
-              <tr key={index}>
-              { 
-                values.map((string, index2) => {
-                  if( Array.isArray(string) )
-                    {
-                    return ( 
-                    string.map((categorie,index3)=> {
-                        return (
-                              <td key={index3}> {categorie.label} </td>
-                              )
-                      })
-
-                     )
-                       
-                    } else if (typeof string === "string"){
-                      if (string.endsWith(".jpeg")|| string.endsWith(".jpg") || string.endsWith(".gif") , string.endsWith(".png") ) {
-                        return (
-                          <td key={index2}><img  src={string}
+    let values;
+    if (this.props.items.length > 0) {
+      this.renderedItems = this.props.items.map((item, index) => {
+        values = Object.values(item);
+        return (
+          <tr key={index}>
+            {values.map((string, index2) => {
+              if (Array.isArray(string)) {
+                return string.map((categorie, index3) => {
+                  return <td key={index3}> {categorie.label} </td>;
+                });
+              } else if (typeof string === "string") {
+                if (
+                  (string.endsWith(".jpeg") ||
+                    string.endsWith(".jpg") ||
+                    string.endsWith(".gif"),
+                  string.endsWith(".png"))
+                ) {
+                  return (
+                    <td key={index2}>
+                      <img
+                        src={string}
                         style={{
-                          width: '100px',
-                          height: '100px',
-                        }} /> </td>
-                        )
-                      }
-                      else  {
-                        
-                      return (
-                              <td key={index2}>{string.toString()}</td>
-                              )
-                    }
-                    } else if (typeof string === "boolean") {
-                      return (
-                              <td key={index2}>{string.toString()}</td>
-                              )
-                    }
-                    
-
-               }
-               )}
-              <td>
-                <Row>
-                  <button className="mr-2 btn-outline-info btn-sm" onClick={this.handleEdit.bind(this, index, item.machineName)}>Edit</button>
-                  <button className='btn btn-outline-danger btn-sm' onClick={this.handleDelete.bind(this, index,item.machineName)}>Delete</button>
-                </Row>
-              </td>
-              </tr>
-              )}
-            )
-      }
-      else {
-        this.renderedItems = null
-      }
-    
+                          width: "100px",
+                          height: "100px"
+                        }}
+                      />{" "}
+                    </td>
+                  );
+                } else {
+                  return <td key={index2}>{string.toString()}</td>;
+                }
+              } else if (typeof string === "boolean") {
+                return <td key={index2}>{string.toString()}</td>;
+              }
+            })}
+            <td>
+              <Row>
+                <button
+                  className="mr-2 btn-outline-info btn-sm"
+                  onClick={this.handleEdit.bind(this, index, item.machineName)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-outline-danger btn-sm"
+                  onClick={this.handleDelete.bind(
+                    this,
+                    index,
+                    item.machineName
+                  )}
+                >
+                  Delete
+                </button>
+              </Row>
+            </td>
+          </tr>
+        );
+      });
+    } else {
+      this.renderedItems = null;
+    }
 
     return (
-      <div className="ViewTable" style={{overflowY: 'scroll', webkitOverflowScrolling: 'touch', maxHeight: '50vh'}}>
+      <div
+        className="ViewTable"
+        style={{
+          overflowY: "scroll",
+          webkitOverflowScrolling: "touch",
+          maxHeight: "50vh"
+        }}
+      >
         <Table striped>
-          <thead>
+          <thead className="tableHead">
             <tr>
               {this.renderedKeys}
               <th>Controllers</th>
             </tr>
           </thead>
-          <tbody>
-          {this.renderedItems}
-            
-          </tbody>
+          <tbody>{this.renderedItems}</tbody>
         </Table>
       </div>
     );
