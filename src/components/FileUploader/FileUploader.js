600339082424aa6f7193f9d388a1c63f7e39d8b9
Flukal <fluviobass@gmail.com>
2018-06-18 11:59:57 +0200

    File Uploader button text

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -1,80 +1,93 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { InputGroup, InputGroupText, InputGroupAddon, Button, Form, FormGroup, Modal,Label, Input, Container, Row, Col, ModalFooter ,ModalBody, ModalHeader} from 'reactstrap';
-import './FileUploader.css';
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Button,
+  Form,
+  FormGroup,
+  Modal,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+  ModalFooter,
+  ModalBody,
+  ModalHeader
+} from "reactstrap";
+import "./FileUploader.css";
 
 export default class FileUploader extends Component {
+  constructor(props) {
+    super(props);
+    let path = "";
+    if (props.path) {
+      path = props.path;
+    }
+    this.state = {
+      description: "",
+      selectedFile: "",
+      imagesArry: [],
+      name: "MediaLibrary",
+      modal: false,
+      ToggleImage: true,
+      Toggleform: false,
+      ShowImageinput: false,
+      border: "",
+      path: path
+    };
+    this.toggle = this.toggle.bind(this);
+    this.toggleNew = this.toggleNew.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/getimages")
+      .then(response => this.setState({ imagesArry: response.data }))
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-    constructor(props) {
-        super(props);
-        let path ='';
-        if(props.path){
-          path= props.path
-        }
-        this.state = {
-          description: '',
-          selectedFile: '',
-          imagesArry: [],
-          name: 'MediaLibrary',
-          modal: false,
-          ToggleImage: true,
-          Toggleform: false,
-          ShowImageinput: false,
-          border: '',
-          path: path
-        }
-        this.toggle = this.toggle.bind(this);
-        this.toggleNew = this.toggleNew.bind(this);
-        this.toggleForm = this.toggleForm.bind(this);
-       
-      }
-      componentDidMount() {
-       
-           axios.get('http://localhost:5000/api/getimages')
-                .then(response => this.setState({imagesArry: response.data})
-            )
-            .catch(function (error) {
-              console.log(error);
-            });
+  onChange = e => {
+    const state = this.state;
+
+    switch (e.target.name) {
+      case "selectedFile":
+        state.selectedFile = e.target.files[0];
+        break;
+      default:
+        state[e.target.name] = e.target.value;
     }
 
-      onChange = (e) => {
-        const state = this.state;
+    this.setState(state);
+  };
 
-        switch (e.target.name) {
-          case 'selectedFile':
-            state.selectedFile = e.target.files[0];
-            break;
-          default:
-            state[e.target.name] = e.target.value;
-        }
-        
-        this.setState(state);
-      }
+  onClick = e => {
+    e.preventDefault();
+    const { description, selectedFile } = this.state;
+    let formData = new FormData();
 
-      onClick = (e) => {
-        e.preventDefault();
-        const { description, selectedFile } = this.state;
-        let formData = new FormData();
+    formData.append("description", description);
+    formData.append("selectedFile", selectedFile);
 
-        formData.append('description', description);
-        formData.append('selectedFile', selectedFile);
-    
-        /*let request = new XMLHttpRequest();
+    /*let request = new XMLHttpRequest();
         request.open('POST','http://localhost:5000/api/upload');
         request.send(formData)*/
 
-        axios.post('http://localhost:5000/api/upload', formData)
-          .then((result) => {
-            console.log(result)
-         })
-        .catch(function(error) {
-        console.log('Error: ', error);
-        });
-          
-      }
+    axios
+      .post("http://localhost:5000/api/upload", formData)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(function(error) {
+        console.log("Error: ", error);
+      });
+  };
 
-      toggle() {
+  toggle() {
     this.setState({
       modal: !this.state.modal
     });
@@ -86,7 +99,7 @@ export default class FileUploader extends Component {
       ToggleImage: true
     });
   }
-toggleForm(e) {
+  toggleForm(e) {
     this.setState({
       ToggleImage: false,
       Toggleform: true
@@ -94,98 +107,116 @@ toggleForm(e) {
   }
 
   handleimage(e) {
-   let imagPth = e.target.src;
-  this.setState({
+    let imagPth = e.target.src;
+    this.setState({
       path: imagPth,
-      border: '2px solid red',
+      border: "2px solid red",
       ShowImageinput: true
     });
-  this.props.bringFileUrl(imagPth,this.props.fieldLabel)
+    this.props.bringFileUrl(imagPth, this.props.fieldLabel);
   }
- 
-      render() {
-       console.log(this.state.path)
-        const { description, selectedFile } = this.state;
-        
-        const allimages = (
-                    <div className="thumbnail">
-                   {this.state.imagesArry.map((image, index) => (
-                        <img src={require('../../../backend/uploads/'+image)} 
-                        key={index}
-                        alt="Lights" 
-                        style={{width: '20%', height: '150px', margin: 20}}
-                        onClick={this.handleimage.bind(this)}
-                         /> 
-                         )) }
-                    </div>
-                 )
-        const uploderForm = (
-                <div>
-                  <input
-                    type="text"
-                    name="description"
-                    value={description}
-                    onChange={this.onChange}
-                  />
-                  <input
-                    type="file"
-                    name="selectedFile"
-                    onChange={this.onChange}
-                  />
-                  <button  onClick={this.onClick} >Upload</button>
-                  </div>
-          )
 
-        return (
-          <div>
-         <div >
-            <InputGroup >
-                <Input  
-                       type="text"
-                        name="description"
-                        className="mr-0"
-                        value={this.state.path}
-                        onChange={this.onChange} />
-                <InputGroupAddon addonType="append">
-                   <Button className="btn btn-primary" onClick={this.toggle}>SelecteFile</Button>
-                </InputGroupAddon>
-              </InputGroup>
-               {this.state.ShowImageinput ? (<img  src={this.state.path}
-                        style={{
-                          width: '100px',
-                          height: '100px',
-                          border: `${this.state.border}`
-                        }} />) : null}
-           </div>
-              <Modal isOpen={this.state.modal} toggle={this.toggle} style={{maxWidth: 1000}}>
-                <ModalHeader toggle={this.toggle}>
-                <div className="ml-5">
-               {!this.state.Toggleform ? (<img  src={this.state.path}
-                        alt="iamge" 
-                        style={{
-                          width: '70px',
-                          height: '60px',
-                          position: 'absolute',
-                          top:5,
-                          right: 100,
-                          border: `${this.state.border}`
+  render() {
+    console.log(this.state.path);
+    const { description, selectedFile } = this.state;
 
-                        }} />) : null}
-                </div>
-                 <Button  color="light" onClick={this.toggleForm}>Uplode New</Button>
-                 <Button  color="light" onClick={this.toggleNew}>Media Library</Button>
-                </ModalHeader>
-                <hr /> 
-                  {this.state.ToggleImage ? allimages : null}
-                  {this.state.Toggleform ? uploderForm : null}
-                  
-                <ModalFooter>
-                  <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                  <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                </ModalFooter>
-              </Modal>
-          
-          </div>
-        );
-      }
- }
+    const allimages = (
+      <div className="thumbnail">
+        {this.state.imagesArry.map((image, index) => (
+          <img
+            src={require("../../../backend/uploads/" + image)}
+            key={index}
+            alt="Lights"
+            style={{ width: "20%", height: "150px", margin: 20 }}
+            onClick={this.handleimage.bind(this)}
+          />
+        ))}
+      </div>
+    );
+    const uploderForm = (
+      <div>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={this.onChange}
+        />
+        <input type="file" name="selectedFile" onChange={this.onChange} />
+        <button onClick={this.onClick}>Upload</button>
+      </div>
+    );
+
+    return (
+      <div>
+        <div>
+          <InputGroup>
+            <Input
+              type="text"
+              name="description"
+              className="mr-0"
+              value={this.state.path}
+              onChange={this.onChange}
+            />
+            <InputGroupAddon addonType="append">
+              <Button className="btn btn-primary" onClick={this.toggle}>
+                Select File
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+          {this.state.ShowImageinput ? (
+            <img
+              src={this.state.path}
+              style={{
+                width: "100px",
+                height: "100px",
+                border: `${this.state.border}`
+              }}
+            />
+          ) : null}
+        </div>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          style={{ maxWidth: 1000 }}
+        >
+          <ModalHeader toggle={this.toggle}>
+            <div className="ml-5">
+              {!this.state.Toggleform ? (
+                <img
+                  src={this.state.path}
+                  alt="iamge"
+                  style={{
+                    width: "70px",
+                    height: "60px",
+                    position: "absolute",
+                    top: 5,
+                    right: 100,
+                    border: `${this.state.border}`
+                  }}
+                />
+              ) : null}
+            </div>
+            <Button color="light" onClick={this.toggleForm}>
+              Uplode New
+            </Button>
+            <Button color="light" onClick={this.toggleNew}>
+              Media Library
+            </Button>
+          </ModalHeader>
+          <hr />
+          {this.state.ToggleImage ? allimages : null}
+          {this.state.Toggleform ? uploderForm : null}
+
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>
+              Do Something
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
